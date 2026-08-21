42f92232656bd4af1049a077b0907d8a2cf6c09a
egor125552 <sitkosergej55@gmail.com>
2026-08-21 21:26:52 +0300

    Route NPC footsteps through true floor-level HRTF

--- web/js/plugins/enemy/footsteps.js
+++ web/js/plugins/enemy/footsteps.js
@@ -7,7 +7,7 @@ export default{
       const personality=enemy.role==="charger"?.86:enemy.role==="sentry"?1.18:enemy.role==="investigator"?.96:1;
       const base=(running?.40:.60)*personality;
       const pitch=(enemy.stepPitch||1)*rand(.97,1.03)*(running?1.04:1);
-      game.audio.play("footstep",{position:{...enemy.position},relative:false,volume,pitch});
+      game.audio.play("footstep",{position:{x:enemy.position.x,y:0,z:enemy.position.z},relative:false,volume,pitch,spatialMode:"hrtf"});
       enemy.nextStep=game.time+base*rand(.88,1.13)+(enemy.stepPhase||0);
       enemy.stepPhase=0;
     });
